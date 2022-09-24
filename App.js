6827eeb518f20cb1b6228c344ef91a3f1d355a24
Erdogan Erdogan <erdoganerdogan@outlook.com>
2022-09-24 21:40:58 +0100

    son commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import React from "react"
-import Info from "./components/Info"
-import About from "./components/About"
-import Interest from "./components/Interest"
-import Footer from "./components/Footer"
+import React from "react";
+import Info from "./components/Info";
+import About from "./components/About";
+import Interest from "./components/Interest";
+import Footer from "./components/Footer";
 
-export default function App (){
-    return (
-<<<<<<< HEAD
-        <div className="App">
-=======
-        <div className="main">
->>>>>>> d1458c2439691cb5f04cd2e5bcd9b54993e5a487
-            <Info />
-            <About />
-            <Interest />
-            <Footer />
-        </div>
-    )
+export default function App() {
+  return (
+    <div className="App">
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
+    </div>
+  );
 }
